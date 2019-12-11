c1b6d7f0b41a6e2792c07490bb39272f3aec3e10
Amanda <jkim@lcs.on.ca>
2019-12-10 21:35:22 -0500

    Initial Commit

---
+++ MultipleChoice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MultipleChoice
+//
+//  Created by Kim, Jenis on 2019-12-10.
+//  Copyright © 2019 Kim, Jenis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
